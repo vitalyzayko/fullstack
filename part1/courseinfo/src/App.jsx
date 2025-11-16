887bff6e75ed9c736e8d0e7d31d48d4dc1e35df4
z0048hzr <vitaly.zayko@siemens-energy.com>
2025-11-16 11:44:55 +0100

    adding part 1, ex 1.1.-1.4

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,73 @@
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+
+
+  const Header = (props) => {
+      return (
+      <>
+        <h1>{props.course}</h1>
+      </>
+      )
+  }
+
+  const Part = (props) => {
+        return (
+            <>
+                <p>
+                    {props.part.name} {props.part.exercises}
+                </p>
+            </>
+        )
+  }
+
+  const Content = (props) => {
+        return (
+        <>
+          <Part part={props.parts[0]} />
+          <Part part={props.parts[1]} />
+          <Part part={props.parts[2]} />
+        </>
+        )
+    }
+
+  const Total = (props) => {
+        let sum = 0
+        props.parts.forEach(value => {
+            sum += value.exercises
+        })
+
+        return (
+        <>
+            <p>
+                Number of exercises {sum}
+            </p>
+        </>
+        )
+  }
+
+
+return (
+      <div>
+        <Header course={course} />
+        <Content parts={parts} />
+        <Total parts={parts} />
+      </div>
+    )
+
+}
+
+export default App
